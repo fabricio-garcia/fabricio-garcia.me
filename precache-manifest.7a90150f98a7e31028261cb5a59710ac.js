a5d28d07d2dc027b855239352cd7b3987814e210
Fabricio <gael.fabricio@gmail.com>
2020-04-14 16:52:08 -0500

    Updates

---
+++ precache-manifest.7a90150f98a7e31028261cb5a59710ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db490e98b40fb7e4db419e4fc763cdf2",
+    "url": "/fabricio-garcia.me/index.html"
+  },
+  {
+    "revision": "93acfbe7c9fc5601b09e",
+    "url": "/fabricio-garcia.me/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "08d01417537aeab85199",
+    "url": "/fabricio-garcia.me/static/js/2.e6023105.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fabricio-garcia.me/static/js/2.e6023105.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93acfbe7c9fc5601b09e",
+    "url": "/fabricio-garcia.me/static/js/main.64d384f3.chunk.js"
+  },
+  {
+    "revision": "62a8c2ed99d6a788bbcb",
+    "url": "/fabricio-garcia.me/static/js/runtime-main.010775e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fabricio-garcia.me/static/media/logo.5d5d9eef.svg"
+  }
+]);
